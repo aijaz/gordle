46e7e34ab6bc630ff37dadb3854dc6e31b88c007
Aijaz Ansari <aijaz@users.noreply.github.com>
2022-11-03 18:57:04 -0600

    Implemented flawed guess processing

--- wordle/wordCheck.go
+++ wordle/wordCheck.go
@@ -1,12 +1,20 @@
 package wordle
 
+import "strings"
+
 func ProcessGuess(secretWord string, guess string) ([5]string, bool) {
 	if secretWord == guess {
-		return [5]string{"🟩", "🟩", "🟩", "🟩", "🟩"}, true // 🟥🟨
+		return [5]string{"🟩", "🟩", "🟩", "🟩", "🟩"}, true
 	}
-	return [5]string{"🟥", "🟥", "🟥", "🟥", "🟥"}, false
-}
-
-func isGuessLengthValid(guess string) bool {
-	return len(guess) == 5
+	result := [5]string{}
+	for i, _ := range guess {
+		if guess[i] == secretWord[i] {
+			result[i] = "🟩"
+		} else if strings.Contains(secretWord, string(guess[i])) {
+			result[i] = "🟨"
+		} else {
+			result[i] = "🟥"
+		}
+	}
+	return result, false
 }
--- wordle/words.go
+++ wordle/words.go
@@ -29,16 +29,6 @@ func GetSecretWord() string {
 	secretWords := strings.Split(string(fileBytes), "\n")
 	index := rand.Intn(len(secretWords))
 	return strings.ToUpper(secretWords[index])
-
-	//
-	//index := rand.Intn(len(validWords))
-	//for k := range validWords {
-	//	if index == 0 {
-	//		return k
-	//	}
-	//	index--
-	//}
-	//return ""
 }
 
 func GetGuess() (string, error) {
@@ -49,33 +39,29 @@ func GetGuess() (string, error) {
 		return "", err
 	}
 	word = strings.ToUpper(word)
-	if !validWords[word] {
-		return "", errors.New("this is not a valid word")
-	}
 	if len(word) != 5 {
 		return "", errors.New("all guesses should be 5 characters in length")
+	}
+	if !validWords[word] {
+		return "", errors.New("this is not a valid word")
 	}
 
 	return word, nil
 }
 
 func populateWords() {
-	validWords["ARISE"] = true
-	validWords["BROOK"] = true
-	validWords["COUNT"] = true
-	validWords["DENIM"] = true
-	validWords["ENJOY"] = true
-	validWords["FLOAT"] = true
-	validWords["OCEAN"] = true
-
 	file, err := os.Open("wordle/all5.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
 
 	scanner := bufio.NewScanner(file)
-	// optionally, resize scanner's capacity for lines over 64K, see next example
 	scanner.Scan()
 	line := scanner.Text()
 	words := strings.Split(line, ",")
